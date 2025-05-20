c5ab30b96d01414edb65a5ce57f8edd86a2fbece
GreyStar <ct37381@sersd.org>
2025-05-20 12:55:40 -0400

    Create 9.3.6 - Adding to a Value.py

---
+++ CodeHS/9/9.3/9.3.6 - Adding to a Value.py
@@ -0,0 +1,6 @@
+num = 10 #creates a variable
+def something(): #creates a function
+    value = int(input("Enter a number: ")) #asks for input, number
+    value = value + num #adds the values
+    print(value) #prints
+something() #calls the function
