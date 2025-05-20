4a18f0165f0dd4447258b441886e0cc22289e1f8
GreyStar <ct37381@sersd.org>
2025-05-20 12:48:16 -0400

    Create 9.2.6 - Print Multiple TImes.py

---
+++ CodeHS/9/9.2/9.2.6 - Print Multiple TImes.py
@@ -0,0 +1,4 @@
+def print_multiple_times(word, num): #creates a function with two arguments
+    for i in range(num): #for loop that repeats the amount of times that was given
+        print(word) #prints the word
+print_multiple_times("Tiger", 5) #calls the function
