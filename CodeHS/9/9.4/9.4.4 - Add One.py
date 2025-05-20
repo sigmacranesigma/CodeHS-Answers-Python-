cb304b7332d1fb45ca2f409119a2031aede71de8
GreyStar <ct37381@sersd.org>
2025-05-20 13:05:18 -0400

    Update 9.4.4 - Add One.py

--- CodeHS/9/9.4/9.4.4 - Add One.py
+++ CodeHS/9/9.4/9.4.4 - Add One.py
@@ -2,4 +2,4 @@ def add_one(): #function that asks for input (number) then return the number + 1
     num = float(input("Enter a number: "))
     num += 1
     return(num)
-print(add_one())
+print(add_one()) #prints the function output
