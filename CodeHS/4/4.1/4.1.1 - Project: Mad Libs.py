d13824d39f1854cbc41c3cfa254df9c35bfcdc7f
GreyStar <ct37381@sersd.org>
2025-05-21 14:28:12 -0400

    Create 4.1.1 - Project: Mad Libs.py

---
+++ CodeHS/4/4.1/4.1.1 - Project: Mad Libs.py
@@ -0,0 +1,9 @@
+print("Welcome to Mad Libs!")
+
+ad = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print("Here's your Mad Libs story: ")
+
+print("Once a upon a time, there was a " + ad + " " + noun + " who loved to " + verb + " every day.")
